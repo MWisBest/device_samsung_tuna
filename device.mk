7577f6d855e8c24436f0260be0d42a801fa0124b
Kyle Repinski <repinski23@gmail.com>
2014-04-01 21:11:05 -0500

    Fix ADB not working in recovery (TWRP, at least).
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 	device/samsung/tuna/ueventd.tuna.rc:root/ueventd.tuna.rc \
 	device/samsung/tuna/media_profiles.xml:system/etc/media_profiles.xml \
 	device/samsung/tuna/media_codecs.xml:system/etc/media_codecs.xml \
-	device/samsung/tuna/gps.conf:system/etc/gps.conf
+	device/samsung/tuna/gps.conf:system/etc/gps.conf \
+	device/samsung/tuna/init.recovery.tuna.rc:root/init.recovery.tuna.rc
 
 # Wifi
 ifneq ($(TARGET_PREBUILT_WIFI_MODULE),)
